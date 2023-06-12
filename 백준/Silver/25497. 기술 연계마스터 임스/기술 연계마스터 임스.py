2b2121c336af2429b21af91bdb8ef880d77afd8c
NAMKIJOO <96736526+namkijoo@users.noreply.github.com>
2023-06-12 16:27:16 +0900

    [Silver V] Title: 기술 연계마스터 임스, Time: 88 ms, Memory: 31448 KB -BaekjoonHub

---
+++ 백준/Silver/25497. 기술 연계마스터 임스/기술 연계마스터 임스.py
@@ -0,0 +1,25 @@
+import sys
+input=sys.stdin.readline
+N=int(input())
+skill = input().rstrip()
+cnt,Ls,Ss=0,0,0
+for i in skill :
+    if i=='L':
+        Ls+=1
+    elif i=='R':
+        if Ls>0 :
+            cnt+=1
+            Ls-=1
+        else:
+            break
+    elif i=='S':
+        Ss+=1
+    elif i=='K':
+        if Ss>0 :
+            cnt+=1
+            Ss-=1
+        else:
+            break
+    else :
+        cnt+=1
+print(cnt)
