42d796809e0de84028ed45328d4674eddb00c874
NAMKIJOO <96736526+namkijoo@users.noreply.github.com>
2023-06-14 15:23:04 +0900

    [Silver IV] Title: 카드, Time: 120 ms, Memory: 34096 KB -BaekjoonHub

---
+++ 백준/Silver/1835. 카드/카드.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+from collections import deque
+N = int(input())
+dq = deque()
+
+dq.appendleft(N)
+
+for n in range(N - 1, 0, -1):
+    dq.appendleft(n)
+
+    for j in range(n):
+        moveCard = dq.pop()
+        dq.appendleft(moveCard)
+
+print(*dq)
