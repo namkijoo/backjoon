5998a1efde600216f496a4175f890bdbdb9b2082
NAMKIJOO <96736526+namkijoo@users.noreply.github.com>
2024-09-06 16:35:03 +0900

    [level 1] Title: 모의고사, Time: 4.01 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,16 +1,27 @@
 function solution(answers) {
-    var answer = [];
-    var a1 = [1, 2, 3, 4, 5];
-    var a2 = [2, 1, 2, 3, 2, 4, 2, 5]
-    var a3 = [ 3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
-
-    var a1c = answers.filter((a,i)=> a === a1[i%a1.length]).length;
-    var a2c = answers.filter((a,i)=> a === a2[i%a2.length]).length;
-    var a3c = answers.filter((a,i)=> a === a3[i%a3.length]).length;
-    var max = Math.max(a1c,a2c,a3c);
-
-    if (a1c === max) {answer.push(1)};
-    if (a2c === max) {answer.push(2)};
-    if (a3c === max) {answer.push(3)};
-    return answer;
+    let answer = [];
+    let patterns=[
+        [1,2,3,4,5],
+        [2,1,2,3,2,4,2,5],
+        [3,3,1,1,2,2,4,4,5,5]
+    ]
+    let score=[0,0,0]
+    for(let i=0; i<answers.length; i++){
+        if(patterns[0][i%5]==answers[i]){
+            score[0]++
+        }
+        if(patterns[1][i%8]==answers[i]){
+            score[1]++
+        }
+        if(patterns[2][i%10]==answers[i]){
+            score[2]++
+        }
+    }
+    let maxNum=Math.max(...score)
+    for (let j=0; j<score.length; j++){
+        if(score[j]==maxNum){
+            answer.push(j+1)
+        }
+    }
+    return answer
 }
