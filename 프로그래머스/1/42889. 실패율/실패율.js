d877be1a7d3372abe755149d5e3219a9b6a5a55c
NAMKIJOO <96736526+namkijoo@users.noreply.github.com>
2024-09-13 16:45:55 +0900

    [level 1] Title: 실패율, Time: 11.95 ms, Memory: 43.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -0,0 +1,23 @@
+function solution(N, stages) {
+    const challenger=new Array(N+2).fill(0);
+    for(const stage of stages){
+        challenger[stage]++
+    }
+    
+    const fails=[];
+    let total = stages.length;
+    
+    for(let i=1; i<N+1; i++){
+        if(challenger[i]===0){
+            fails[i]=0
+            continue;
+        }
+        
+        fails[i]=challenger[i]/total;
+        total -= challenger[i]
+    }
+    
+    const result=Object.entries(fails).sort((a,b)=>b[1]-a[1])
+    
+    return result.map((v)=>Number(v[0]))
+}
