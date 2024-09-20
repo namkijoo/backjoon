8e79b0c6550e1285fba64877df3363ec56df1a99
NAMKIJOO <96736526+namkijoo@users.noreply.github.com>
2024-09-20 17:41:45 +0900

    [level 2] Title: 괄호 회전하기, Time: 547.29 ms, Memory: 37.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -0,0 +1,44 @@
+function solution(s) {
+    let answer = s;
+    let result = 0;
+    
+    for (let i = 0; i < answer.length; i++) {
+        if (isTrue(answer)) {
+            result += 1;
+        }
+        answer = changeString(answer);
+    }
+    
+    return result;
+}
+
+function changeString(answers) {
+    let arr = [];
+    for (let i = 1; i < answers.length; i++) {
+        arr.push(answers[i]);
+    }
+    arr.push(answers[0]);
+    return arr.join(''); 
+}
+
+function isTrue(answers) {
+    const arr = [];
+    for (let i = 0; i < answers.length; i++) {
+        if (answers[i] === "(" || answers[i] === "{" || answers[i] === "[") {
+            arr.push(answers[i]);
+        } else if (answers[i] === ")") {
+            if (arr.length === 0 || arr.pop() !== "(") {
+                return false;
+            }
+        } else if (answers[i] === "}") {
+            if (arr.length === 0 || arr.pop() !== "{") {
+                return false;
+            }
+        } else if (answers[i] === "]") {
+            if (arr.length === 0 || arr.pop() !== "[") {
+                return false;
+            }
+        }
+    }
+    return arr.length === 0;  
+}
