d03e324efdcd81c7273701e0c8eae97094e37eef
NAMKIJOO <96736526+namkijoo@users.noreply.github.com>
2024-09-27 14:05:58 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.53 ms, Memory: 43 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -1,12 +1,10 @@
 function solution(arr) {
     var answer = [];
-    
     if (arr.length <= 1) {
         return [-1];
     } else {
         arr.splice(arr.indexOf(Math.min(...arr)),1);
-        answer = arr;
     }
     
-    return answer;
+    return arr
 }
