f4ac7a991e00e286849820a4e308cd26306ea76e
NAMKIJOO <96736526+namkijoo@users.noreply.github.com>
2024-10-02 16:50:09 +0900

    [level 2] Title: 기능개발, Time: 0.21 ms, Memory: 32.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -0,0 +1,23 @@
+function solution(progresses, speeds) {
+    var answer = [];
+
+    const daysLeft=progresses.map((progress,index)=>(
+        Math.ceil((100-progress)/speeds[index])
+    ))
+    
+    let count =0;
+    let maxDay=daysLeft[0]
+    
+    for(let i=0; i<progresses.length; i++){
+        if(daysLeft[i]<=maxDay){
+            count++
+        }else{
+            answer.push(count)
+            count=1
+            maxDay=daysLeft[i]
+        }
+    }
+    
+    answer.push(count)
+    return answer;
+}
