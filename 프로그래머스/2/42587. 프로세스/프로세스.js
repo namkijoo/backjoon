72502399f88d009f69ea1464414d46050b5a1b7a
NAMKIJOO <96736526+namkijoo@users.noreply.github.com>
2024-10-10 03:20:51 +0900

    [level 2] Title: 프로세스, Time: 0.41 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -0,0 +1,24 @@
+function solution(priorities, location) {
+    let answer = 0;
+    let arr = []
+    let max_value = Math.max(...priorities);
+
+    //위치 배열 만들기
+    for(let i = 0; i < priorities.length; i++){
+        arr.push(i);
+    }
+
+    while(priorities.length != 0){
+         max_value = Math.max(...priorities);
+        
+        if(priorities[0] < max_value){
+            priorities.push(priorities.shift());
+            arr.push(arr.shift());
+        }else {
+            answer+=1;
+            priorities.shift();
+            if(arr.shift() == location)
+                return answer;
+        }
+    }
+}
