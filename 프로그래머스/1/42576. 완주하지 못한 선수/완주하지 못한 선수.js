e035cda9add2f66676168599415a109a56e9f8cb
NAMKIJOO <96736526+namkijoo@users.noreply.github.com>
2024-10-29 02:21:33 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 61.74 ms, Memory: 65.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,11 +1,23 @@
 function solution(participant, completion) {
-    participant.sort()
-    completion.sort()
-    
-    for(let i=0; i<completion.length; i++){
-        if(participant[i]!==completion[i]){
-            return participant[i]
+    const myMap = new Map();
+
+    for (const p of participant) {
+        if (!myMap.has(p)) {
+            myMap.set(p, 1);
+        } else {
+            myMap.set(p, myMap.get(p) + 1);
         }
     }
-    return participant[participant.length-1]
+
+    for (const c of completion) {
+        if (myMap.has(c)) {
+            myMap.set(c, myMap.get(c) - 1);
+        }
+    }
+
+    for (const p of participant) {
+        if (myMap.get(p) >= 1) {
+            return p; 
+        }
+    }
 }
