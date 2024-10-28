ef64104360d51991b1f5d250c9abeb241702d9a0
NAMKIJOO <96736526+namkijoo@users.noreply.github.com>
2024-10-29 01:53:26 +0900

    [level 1] Title: 폰켓몬, Time: 0.68 ms, Memory: 34.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,5 +1,5 @@
 function solution(nums) {
     const numSet = new Set(nums);
-    const halfNumsLength = parseInt(nums.length / 2);
+    const halfNumsLength = (nums.length / 2);
     return numSet.size > halfNumsLength ? halfNumsLength : numSet.size;
 }
