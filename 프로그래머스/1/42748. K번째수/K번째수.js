4d66219f4191a2c45af4f5096b302e1ef9aca3a6
NAMKIJOO <96736526+namkijoo@users.noreply.github.com>
2024-10-30 16:11:19 +0900

    [level 1] Title: K번째수, Time: 3.43 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,9 +1,10 @@
 function solution(array, commands) {
-    var answer = [];
-    for(let i=0; i<commands.length; i++){
-        console.log(array.slice(commands[i][0]-1,commands[i][1]).sort((a,b)=>a-b)) 
-        let result=array.slice(commands[i][0]-1,commands[i][1]).sort((a,b)=>a-b)
-        answer.push(result[commands[i][2]-1])
-    }
+    const answer=[]
+   for(let i=0; i<commands.length; i++){
+       let sliceCommands=array.slice(commands[i][0]-1,commands[i][1])
+       console.log(sliceCommands)
+       let sortedCommands=sliceCommands.sort((a,b)=>a-b)
+       answer.push(sortedCommands[commands[i][2]-1])
+   }
     return answer
 }
