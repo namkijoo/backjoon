b7fa27a7083806364ca0fe17f84f8ed9f72364ea
NAMKIJOO <96736526+namkijoo@users.noreply.github.com>
2024-11-04 15:16:44 +0900

    [level 2] Title: 타겟 넘버, Time: 66.85 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,24 +1,25 @@
 function solution(numbers, target) {
-    var answer = 0;
+    const queue = [];
+    let answer = 0;
     
-    
-    function dfs(index,sum){
-        
-        if(index === numbers.length){
-            if(sum === target){
+    // 초기값 설정: (인덱스, 합계)
+    queue.push([0, 0]);
+
+    while (queue.length > 0) {
+        const [l, sum] = queue.pop();
+
+        // 모든 numbers 배열의 요소를 처리했을 때
+        if (l === numbers.length) {
+            if (sum === target) {
                 answer++;
-               
             }
-             return;
+        } 
+        // 그렇지 않다면, 현재 요소의 값을 더하거나 빼서 새로운 합계를 구하고 큐에 추가
+        else {
+            queue.push([l + 1, sum + numbers[l]]);
+            queue.push([l + 1, sum - numbers[l]]);
         }
-        
-        dfs(index+1, sum + numbers[index]);
-        dfs(index+1, sum - numbers[index]);
-        
     }
-    
-    dfs(0,0);
-    
-    
+
     return answer;
 }
