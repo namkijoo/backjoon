02c9ff3b6201ed0fa089a4e4dc1f606be6e1acb1
NAMKIJOO <96736526+namkijoo@users.noreply.github.com>
2024-11-06 20:48:51 +0900

    [Silver III] Title: 바이러스, Time: 92 ms, Memory: 9436 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.js
+++ 백준/Silver/2606. 바이러스/바이러스.js
@@ -4,7 +4,7 @@ const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 const length = Number(input[0]);
 const lengthNum = Number(input[1]);
 
-// 그래프를 인접 리스트 형태로 초기화
+// 그래프 생성
 const graph = Array.from({ length: length + 1 }, () => []);
 for (let i = 2; i < lengthNum + 2; i++) {
     const [n, m] = input[i].split(" ").map(Number);
@@ -15,24 +15,18 @@ for (let i = 2; i < lengthNum + 2; i++) {
 const visited = Array(length + 1).fill(false);
 let result = 0;
 
-function bfs(start) {
-    const queue = [start];
-    visited[start] = true;
-
-    while (queue.length > 0) {
-        const node = queue.shift();
-
-        for (const next of graph[node]) {
-            if (!visited[next]) {
-                visited[next] = true;
-                queue.push(next);
-                result += 1;
-            }
+// DFS 탐색 함수
+function dfs(node) {
+    visited[node] = true; 
+    for (const next of graph[node]) {
+        if (!visited[next]) {
+            result++;  
+            dfs(next);  
         }
     }
 }
 
-// 1번 컴퓨터부터 시작
-bfs(1);
+visited[1] = true;  
+dfs(1);
 
 console.log(result);
