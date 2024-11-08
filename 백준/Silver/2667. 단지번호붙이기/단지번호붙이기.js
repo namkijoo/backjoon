6f4e2ec3fdffc3a97e6d65b94b316f2da5401973
NAMKIJOO <96736526+namkijoo@users.noreply.github.com>
2024-11-09 00:27:41 +0900

    [Silver I] Title: 단지번호붙이기, Time: 148 ms, Memory: 10148 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.js
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.js
@@ -7,28 +7,36 @@ const dirs = [[1, 0], [-1, 0], [0, 1], [0, -1]];
 const visited = Array.from({ length: n }, () => Array(n).fill(false));  
 const result = [];
 
-// DFS 함수 정의
-function dfs(x, y) {
+
+function bfs(x, y) {
     let count = 1;  
     visited[x][y] = true;
+    const queue = [[x, y]];
 
-    for (const [dx, dy] of dirs) {
-        const nx = x + dx;
-        const ny = y + dy;
+    while (queue.length > 0) {
+        const [currentX, currentY] = queue.shift();
+        
+        for (const [dx, dy] of dirs) {
+            const nx = currentX + dx;
+            const ny = currentY + dy;
 
-        if (nx >= 0 && nx < n && ny >= 0 && ny < n && list[nx][ny] === 1 && !visited[nx][ny]) {
-            count += dfs(nx, ny);  
+            if (nx >= 0 && nx < n && ny >= 0 && ny < n && list[nx][ny] === 1 && !visited[nx][ny]) {
+                visited[nx][ny] = true;
+                queue.push([nx, ny]);
+                count += 1;
+            }
         }
     }
 
     return count;
 }
 
+
 // 단지 찾기
 for (let i = 0; i < n; i++) {
     for (let j = 0; j < n; j++) {
         if (list[i][j] === 1 && !visited[i][j]) {  
-            const housesInComplex = dfs(i, j);
+            const housesInComplex = bfs(i, j);
             result.push(housesInComplex);
         }
     }
