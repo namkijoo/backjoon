25af6fc1fd4a37741e63bbd634684f3aadfa87d0
NAMKIJOO <96736526+namkijoo@users.noreply.github.com>
2024-12-13 00:21:34 +0900

    [level 2] Title: 주식가격, Time: 32.39 ms, Memory: 41.9 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.js
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -1,14 +1,19 @@
 function solution(prices) {
-    const answer = [];
+    const answer = Array(prices.length).fill(0);
+    const stack = []; 
+
     for (let i = 0; i < prices.length; i++) {
-        let stack = 0;
-        for (let j = i + 1; j < prices.length; j++) {
-            stack++;
-            if (prices[i] > prices[j]) {
-                break;
-            }
+        while (stack.length > 0 && prices[stack[stack.length - 1]] > prices[i]) {
+            const topIndex = stack.pop();
+            answer[topIndex] = i - topIndex; 
         }
-        answer.push(stack);
+        stack.push(i); 
     }
+
+    while (stack.length > 0) {
+        const topIndex = stack.pop();
+        answer[topIndex] = prices.length - 1 - topIndex;
+    }
+
     return answer;
 }
