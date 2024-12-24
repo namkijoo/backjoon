9bec2f0681b64d3f4416628c971f7d21fed6da67
NAMKIJOO <96736526+namkijoo@users.noreply.github.com>
2024-12-24 16:02:05 +0900

    [level 2] Title: 전화번호 목록, Time: 294.94 ms, Memory: 92.1 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,19 +1,18 @@
 function solution(phone_book) {
-    let hash={}
-    
-    for(const a of phone_book){
-        hash[a]=true
-    }
+   const hashMap = new Map()
+   
+   for(const a of phone_book){
+       hashMap.set(a,true)
+   }
 
     for(const a of phone_book){
         for(let i=1; i<a.length; i++){
-            let prefix = a.substring(0,i)
-            if(hash[prefix]){
+            let prefix= a.substring(0,i)
+            if(hashMap.has(prefix)){
                 return false
             }
         }
     }
-    
     return true
 }
 
